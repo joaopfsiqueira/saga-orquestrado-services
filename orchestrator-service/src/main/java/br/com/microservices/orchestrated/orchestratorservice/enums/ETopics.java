2a62caddf4a24c7a97100b31c2acafb2da917f8f
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-08 23:56:06 -0300

    feat: CREATING class to keep topics name

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/enums/ETopics.java
@@ -0,0 +1,25 @@
+package br.com.microservices.orchestrated.orchestratorservice.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter // retornar só os atributos
+@AllArgsConstructor // gera constructor com todos os argumetos
+public enum ETopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAILURE("finish-failure"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAILURE("product-validation-failure"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAILURE("inventory-failure"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAILURE("payment-failure"),
+    NOTIFY_ENDING("notify-ending");
+
+
+    private String Topic;
+}
