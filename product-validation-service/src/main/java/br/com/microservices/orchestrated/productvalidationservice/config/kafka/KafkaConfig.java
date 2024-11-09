d5bb2ebb75999bc5d5d932fa7ddba597e50982a3
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-09 08:51:57 -0300

    feat: criando tópico PRODUCT VALIDATION

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/config/kafka/KafkaConfig.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/config/kafka/KafkaConfig.java
@@ -1,6 +1,7 @@
 package br.com.microservices.orchestrated.productvalidationservice.config.kafka;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -19,12 +21,23 @@ import java.util.Map;
 @RequiredArgsConstructor // cria um construtor com todos os atributos final
 public class KafkaConfig {
 
+    private static final Integer PARTITION_COUNT = 1;
+    private static final Integer REPLICA_COUNT = 1;
+
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String boostrapServer;
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+    @Value("${spring.kafka.topic.product-validation-success}")
+    private String productValidationSuccessTopic;
+    @Value("${spring.kafka.topic.product-validation-failure}")
+    private String productValidationFailureTopic;
+
 
     @Bean // informando para o spring que esse metodo vai ser CONFIGURÁVEL, bean é um objeto insganciado.
     public ConsumerFactory<String, String> consumerFactory() {
@@ -60,6 +73,26 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+
+    private NewTopic buildTopic(String name){
+        return TopicBuilder.name(name).replicas(REPLICA_COUNT).partitions(PARTITION_COUNT).build();
+    }
+
+    @Bean
+    public NewTopic orchestratorTopicTopic() {
+        return buildTopic(orchestratorTopic);
+    }
+
+    @Bean
+    public NewTopic productValidationSuccessTopic() {
+        return buildTopic(productValidationSuccessTopic);
+    }
+
+    @Bean
+    public NewTopic productValidationFailureTopic() {
+        return buildTopic(productValidationFailureTopic);
     }
 
 }
