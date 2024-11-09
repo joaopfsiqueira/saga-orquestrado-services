cc62f063ee120c2396d305991da5cc3e6e8a6acc
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-09 08:00:16 -0300

    feat: ajustando producerprops

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/config/KafkaConfig.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/config/KafkaConfig.java
@@ -2,7 +2,9 @@ package br.com.microservices.orchestrated.orderservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,8 +40,8 @@ public class KafkaConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // deserializa a chave, JA QUE É O CONSUMER
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); // deserializa o valor, JA QUE É O CONSUMER
         return props;
     }
 
@@ -50,11 +52,9 @@ public class KafkaConfig {
 
     private Map<String, Object> producerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // SERELIAZA A CHAVE, JA QUE É O PRODUCER
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // SERELIAZA O VALOR, JA QUE É O PRODUCER
         return props;
     }
 
