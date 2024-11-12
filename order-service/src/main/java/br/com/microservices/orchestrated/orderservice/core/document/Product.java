dc3194246b8619024f7a78aaabb2d3afd04630e5
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-12 09:36:09 -0300

    feat: preenchendo product

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Product.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Product.java
@@ -1,4 +1,14 @@
 package br.com.microservices.orchestrated.orderservice.core.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // criar getters e setters
+@NoArgsConstructor // criar construtor vazio
+@AllArgsConstructor // criar construtor com todos os atributos
 public class Product {
+
+    private String code;
+    private double unitValue;
 }
