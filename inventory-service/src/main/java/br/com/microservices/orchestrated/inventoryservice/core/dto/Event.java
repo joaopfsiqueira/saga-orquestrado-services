6b2b24ab6f5b6cc5e1cc0a2696094a26ac2fc1d4
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-12 11:11:18 -0300

    feat: levando os dtos para inventory

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/Event.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.inventoryservice.core.dto;
+
+
+
+import br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data // criar getters e setters
+@NoArgsConstructor // criar construtor vazio
+@AllArgsConstructor // criar construtor com todos os atributos
+@Builder //
+public class Event {
+
+    private String id;
+    private String transactionId;
+    private String orderId;
+    private Order order;
+    private String source;
+    private ESagaStatus status;
+    private List<History> eventHistory;
+    private LocalDateTime createdAt;
+}
---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/OrderProduct.java
@@ -0,0 +1,15 @@
+package br.com.microservices.orchestrated.inventoryservice.core.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // criar getters e setters
+@NoArgsConstructor // criar construtor vazio
+@AllArgsConstructor // criar construtor com todos os atributos
+public class OrderProduct {
+
+    private Product product;
+    private int quantity;
+}
