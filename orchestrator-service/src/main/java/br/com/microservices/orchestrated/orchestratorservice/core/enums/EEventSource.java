3dae06a8626424d46789fc68cc0691ff09240d5a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-12 09:54:40 -0300

    feat: preenchendo enum de source

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EEventSource.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EEventSource.java
@@ -1,4 +1,9 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.enums;
 
+// Enum que representa a origem do evento
 public enum EEventSource {
+    ORCHESTRATOR,
+    PRODUCT_VALIDATION_SERVICE,
+    PAYMENT_SERVICE,
+    INVENTORY_SERVICE
 }
