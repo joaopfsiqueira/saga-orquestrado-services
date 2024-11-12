e327b5e4a5e3a5e0f7d9455458243096020415d9
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-12 11:07:52 -0300

    fix: ajustando status

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/History.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/History.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.dto;
 
+import br.com.microservices.orchestrated.productvalidationservice.core.enums.ESagaStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 public class History {
 
     private String source;
-    private String status;
+    private ESagaStatus status;
     private String message;
     private LocalDateTime createdAt;
 }
