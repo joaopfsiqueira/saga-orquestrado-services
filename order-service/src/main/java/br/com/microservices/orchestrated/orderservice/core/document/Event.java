28e40c2351a47dc8f5cf4e2f3f20bfae392789d0
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-12 09:43:22 -0300

    feat: criando classe que representa no mongo o event

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.orderservice.core.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data // criar getters e setters
+@NoArgsConstructor // criar construtor vazio
+@AllArgsConstructor // criar construtor com todos os atributos
+@Builder //
+public class Event {
+
+    private String id;
+    private String transactionId;
+    private String orderId;
+    private Order order;
+    private String source;
+    private String status;
+    private List<History> eventHistory;
+    private LocalDateTime createdAt;
+}
