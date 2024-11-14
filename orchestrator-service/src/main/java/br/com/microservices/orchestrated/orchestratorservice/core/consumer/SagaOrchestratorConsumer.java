af68958f5c67981f3f21784b3090363cb374c9ac
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-14 09:13:17 -0300

    feat: criando classe de consumer para o kafka

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/consumer/SagaOrchestratorConsumer.java
@@ -0,0 +1,57 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.consumer;
+
+import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class SagaOrchestratorConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.start-saga}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumeStartSagaEvent(String payload) {
+        log.info("Receiving  event {} from start-saga topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.orchestrator}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumeOrchestratorEvent(String payload) {
+        log.info("Receiving event {} from orchestrator topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-success}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumeFinishSuccessEvent(String payload) {
+        log.info("Receiving event {} from finish-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.finish-failure}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumeFinishFailureEvent(String payload) {
+        log.info("Receiving event {} from finish-failure topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+
+}
