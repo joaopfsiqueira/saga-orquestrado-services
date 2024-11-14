0347d8c19ca2c32430be9c62cd2b4c294cd5c5a7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-14 09:17:37 -0300

    feat: criando classe para consumo do kafka

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/consumer/PaymentConsumer.java
@@ -0,0 +1,37 @@
+package br.com.microservices.orchestrated.paymentservice.core.consumer;
+
+import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class PaymentConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-validation-success}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumePaymentValidationSuccessEvent(String payload) {
+        log.info("Receiving success event {} from payment-validation-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.payment-validation-failure}"
+    ) //esse metodo é um kafka listerner! uma função que consome do kafka
+    public void ConsumePaymentValidationFailureEvent(String payload) {
+        log.info("Receiving rollback event {} from payment-validation-failure topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+
+}
