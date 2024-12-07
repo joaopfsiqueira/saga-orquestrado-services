483232e3597d4194767fe1d7647e77bf53fc4e15
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-07 16:39:08 -0300

    feat: criando collection order

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,8 +14,10 @@ import java.util.List;
 @NoArgsConstructor // criar construtor vazio
 @AllArgsConstructor // criar construtor com todos os atributos
 @Builder //
+@Document(collection = "order")
 public class Order {
 
+    @Id
     private String id;
     private List<OrderProduct> products;
     private LocalDateTime createdAt;
