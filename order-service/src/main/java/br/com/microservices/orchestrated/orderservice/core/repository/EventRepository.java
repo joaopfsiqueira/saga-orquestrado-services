cd9c3f404e5cba978e26c72df9cb2836e02834bc
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-09 08:55:02 -0300

    feat: criando query method

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -5,5 +5,9 @@ import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.document.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface EventRepository extends MongoRepository<Event, String> {
+
+    List<Event> findAllByOrderByCreatedAtDesc();
 }
