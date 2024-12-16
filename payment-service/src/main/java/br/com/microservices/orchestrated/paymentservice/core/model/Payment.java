03c99a2f1a3e45cf4a84f51d88300e44e2807d2f
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-16 15:19:04 -0300

    feat: criando model payment

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/model/Payment.java
@@ -0,0 +1,56 @@
+package br.com.microservices.orchestrated.paymentservice.core.model;
+
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera um valor automaticamente
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private int totalItems;
+
+    @Column(nullable = false)
+    private double totalAmount;
+
+    @Column(nullable = false)
+    private EPaymentStatus paymentStatus;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist // Executa antes de salvar
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate // caso ja exista, só atualiza o update
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
