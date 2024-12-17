3f50a100b1c26462cdc138dcf29556a01f88370b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-17 13:00:39 -0300

    feat: order inventory model

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -0,0 +1,50 @@
+package br.com.microservices.orchestrated.inventoryservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_inventory")
+public class OrderInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera um valor automaticamente
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", nullable = false)
+    private Inventory inventory;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist // Executa antes de salvar
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate // caso ja exista, só atualiza o update
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
