8d7e5daa5ce3fe888d7a1f3e559591eab3ff3865
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-26 16:25:23 -0300

    feat: finalizando inventory consumer

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,26 +12,27 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class InventoryConsumer {
 
+    private final InventoryService inventoryService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topic.inventory-success}"
     ) //esse metodo é um kafka listerner! uma função que consome do kafka
-    public void ConsumeInventoryValidationSuccessEvent(String payload) {
+    public void ConsumeSuccessEvent(String payload) {
         log.info("Receiving success event {} from inventory-success topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topic.inventory-failure}"
     ) //esse metodo é um kafka listerner! uma função que consome do kafka
-    public void ConsumeInventoryValidationFailureEvent(String payload) {
+    public void ConsumeFailureEvent(String payload) {
         log.info("Receiving rollback event {} from inventory-failure topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 
 
