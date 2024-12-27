40964328d817f19e3e31ca3413ab65de8bb1a8c4
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-27 17:40:50 -0300

    FEAT: lógica que define o tópico dependendo do status

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaHandler.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaHandler.java
@@ -14,7 +14,23 @@ public final class SagaHandler {
     // matriz de objetos, onde cada objeto é um array de objetos
     public static final Object[][] SAGA_HANDLER = {
             {EEventSource.ORCHESTRATOR, ESagaStatus.SUCCESS, ETopics.PRODUCT_VALIDATION_SUCCESS},
-            {1, 2, 1},
-            {2, 2, 1}
+            {EEventSource.ORCHESTRATOR, ESagaStatus.FAIL, ETopics.FINISH_FAILURE},
+
+            {EEventSource.PRODUCT_VALIDATION_SERVICE, ESagaStatus.ROLLBACK_PENDING, ETopics.PRODUCT_VALIDATION_FAILURE},
+            {EEventSource.PRODUCT_VALIDATION_SERVICE, ESagaStatus.FAIL, ETopics.FINISH_FAILURE},
+            {EEventSource.PRODUCT_VALIDATION_SERVICE, ESagaStatus.SUCCESS, ETopics.PAYMENT_SUCCESS},
+
+            {EEventSource.PAYMENT_SERVICE, ESagaStatus.ROLLBACK_PENDING, ETopics.PAYMENT_FAILURE},
+            {EEventSource.PAYMENT_SERVICE, ESagaStatus.FAIL, ETopics.PRODUCT_VALIDATION_FAILURE},
+            {EEventSource.PAYMENT_SERVICE, ESagaStatus.SUCCESS, ETopics.INVENTORY_SUCCESS},
+
+            {EEventSource.INVENTORY_SERVICE, ESagaStatus.ROLLBACK_PENDING, ETopics.INVENTORY_FAILURE},
+            {EEventSource.INVENTORY_SERVICE, ESagaStatus.FAIL, ETopics.PAYMENT_FAILURE},
+            {EEventSource.INVENTORY_SERVICE, ESagaStatus.SUCCESS, ETopics.FINISH_SUCCESS},
     };
+
+    public static final int EVENT_SOURCE_INDEX = 0;
+    public static final int SAGA_STATUS_INDEX = 1;
+    public static final int TOPIC_INDEX = 2;
+
 }
