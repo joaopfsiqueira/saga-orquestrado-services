a6015c018f882f62462b493d4c428acbfff159cb
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-12-30 11:56:43 -0300

    FEAT: finalizando SagaExecutionController

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaExecutionController.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/saga/SagaExecutionController.java
@@ -7,7 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
+import static br.com.microservices.orchestrated.orchestratorservice.core.saga.SagaHandler.*;
 import static org.springframework.util.ObjectUtils.isEmpty;
+import static java.lang.String.format;
 
 @Slf4j
 @Component
@@ -15,10 +19,47 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 public class SagaExecutionController {
 
 
+    private static final String SAGA_LOG_ID = "ORDER ID: %s | TRANSACTION ID %s | EVENT ID %s";
+
     public ETopics getNextTopic(Event event) {
-        if(isEmpty(event.getStatus()) || isEmpty(event.getSource())) {
-           throw new ValidationException("Event status or source is empty and must be informed!");
+        if (isEmpty(event.getSource()) || isEmpty(event.getStatus())) {
+            throw new ValidationException("Source and status must be informed.");
         }
-        return ETopics.BASE_ORCHESTRATOR;
+        var topic = findTopicBySourceAndStatus(event);
+        logCurrentSaga(event, topic);
+        return topic;
+    }
+
+    private ETopics findTopicBySourceAndStatus(Event event) {
+        return (ETopics) (Arrays.stream(SAGA_HANDLER)
+                .filter(row -> isEventSourceAndStatusValid(event, row))
+                .map(i -> i[TOPIC_INDEX])
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Topic not found!")));
+    }
+
+    private boolean isEventSourceAndStatusValid(Event event,
+                                                Object[] row) {
+        var source = row[EVENT_SOURCE_INDEX];
+        var status = row[SAGA_STATUS_INDEX];
+        return source.equals(event.getSource()) && status.equals(event.getStatus());
+    }
+
+    private void logCurrentSaga(Event event, ETopics topic) {
+        var sagaId = createSagaId(event);
+        var source = event.getSource();
+        switch (event.getStatus()) {
+            case SUCCESS -> log.info("### CURRENT SAGA: {} | SUCCESS | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+            case ROLLBACK_PENDING -> log.info("### CURRENT SAGA: {} | SENDING TO ROLLBACK CURRENT SERVICE | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+            case FAIL -> log.info("### CURRENT SAGA: {} | SENDING TO ROLLBACK PREVIOUS SERVICE | NEXT TOPIC {} | {}",
+                    source, topic, sagaId);
+        }
+    }
+
+    private String createSagaId(Event event) {
+        return format(SAGA_LOG_ID,
+                event.getOrder().getId(), event.getTransactionId(), event.getId());
     }
 }
